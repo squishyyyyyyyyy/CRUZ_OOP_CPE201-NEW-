a15d02129ab1b64c007cdb611d8eb0776f7f0091
Ken Cruz <kenlcruz088@gmail.com>
2026-02-02 14:23:34 +0800

    update 3

--- CRUZ_OOP_CPE201/Activity 2.Designer.cs
+++ CRUZ_OOP_CPE201/Activity 2.Designer.cs
@@ -400,7 +400,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MV Boli", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(128, 27);
+            this.label1.Location = new System.Drawing.Point(155, 21);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(498, 58);
